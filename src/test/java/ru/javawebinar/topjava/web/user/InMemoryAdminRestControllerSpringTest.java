09b5f8c881c8c09bb2cd948266b454f4447589b2
Senichek <senichek@gmail.com>
2020-10-18 19:43:15 +0200

    HW03 Optional in progress. Tomcat, SpringMain, InMemoryAdminRestControllerTest, InMemoryAdminRestControllerSpringTest and UserServiceTest start OK.

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -13,9 +13,12 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration({
+        "classpath:spring/test-spring-app.xml"
+})
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
+
 
     @Autowired
     private AdminRestController controller;
